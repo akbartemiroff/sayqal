937b66a5ee674b9fc481d5c6031b8cf4ed59201e
User <user@example.com>
2025-04-04 20:53:19 +0500

    Updates

---
+++ _next/static/WFgLelmYDxJeBbxyqFBSh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/996-80bd74c565659df8.js","static/chunks/pages/index-16272f26f1e8f379.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
